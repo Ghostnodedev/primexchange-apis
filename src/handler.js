0ad0894f25da0132d74ec6f8cdb5f4cf3c8dac45
vaibhav@14 <vaibhavpandey331@gmail.com>
2025-08-16 00:00:56 +0530

    method removed and code uncommented

--- src/handler.js
+++ src/handler.js
@@ -47,20 +47,20 @@ export const register = async (req, res) => {
 // src/handler.js
 
 
-// export const getcrypto = async (req, res) => {
-//   if (req.method === 'GET') {
-//     try {
-//       const response = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
-//       const data = await response.json();
-//       return res.status(200).json(data);
-//     } catch (err) {
-//       return res.status(500).json({ error: err.message });
-//     }
-//   } else {
-//     res.setHeader('Allow', ['GET']);
-//     return res.status(405).json({ error: `Method ${req.method} Not Allowed` });
-//   }
-// }
+export const getcrypto = async (req, res) => {
+  if (req.method === 'GET') {
+    try {
+      const response = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+      const data = await response.json();
+      return res.status(200).json(data);
+    } catch (err) {
+      return res.status(500).json({ error: err.message });
+    }
+  } else {
+    res.setHeader('Allow', ['GET']);
+    return res.status(405).json({ error: `Method ${req.method} Not Allowed` });
+  }
+}
 
 export function test(req, res) {
   if (req.method === "GET") {
