e8e5114cd3def96e146cde4ffe86e6b6357e20a2
vaibhav@14 <vaibhavpandey331@gmail.com>
2025-08-15 23:38:16 +0530

    remove default

--- src/handler.js
+++ src/handler.js
@@ -47,7 +47,7 @@ export const register = async (req, res) => {
 // src/handler.js
 
 
-export default async function handler(req, res) {
+export async function handler(req, res) {
   const { method, url } = req;
 
   console.log('Incoming:', method, url);
