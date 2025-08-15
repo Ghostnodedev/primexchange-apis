4717937cfe50386fc7da38dcde4befc1369f9507
vaibhav@14 <vaibhavpandey331@gmail.com>
2025-08-15 23:39:21 +0530

    function name chaged

--- src/handler.js
+++ src/handler.js
@@ -47,7 +47,7 @@ export const register = async (req, res) => {
 // src/handler.js
 
 
-export async function handler(req, res) {
+export async function getcrypto(req, res) {
   const { method, url } = req;
 
   console.log('Incoming:', method, url);
