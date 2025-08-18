3cab414d55d408e9d1f1d95c0e964b64c6244518
vaibhav@14 <vaibhavpandey331@gmail.com>
2025-08-19 00:06:43 +0530

    checking reg and login data

--- src/handler.js
+++ src/handler.js
@@ -27,29 +27,39 @@ export default async function handler(req, res) {
 
   const pathname = url.split("?")[0]; // strip query params
 
-  const data = []
-
-  if (pathname === "/login" && method === "POST") {
-    const { username, password, email, phone } = req.body || {};
-    if (!username || !password || !email || !phone) {
-      return res.status(400).json({ message: "Missing fields" });
-    }
-
-    data.push({ username, email, phone });
-    return res.status(200).json({ message: "Login successful", user: { username } });
-  }
-
-  // REGISTER
+    // REGISTER
+  const regdata = []
   if (pathname === "/register" && method === "POST") {
-    const { name, email, username, password, confirmpassword, phone, age } = req.body || {};
+    const { name, email, username, password, confirmpassword, phone } = req.body || {};
     if (!name || !email || !username || !password || !confirmpassword || !phone || !age) {
       return res.status(400).json({ message: "Missing required fields" });
     }
     if (password !== confirmpassword) {
       return res.status(400).json({ message: "Passwords do not match" });
     }
+    regdata.push({ name, email, username, phone });
+    console.log("Registration data:", regdata);
     return res.status(201).json({ message: "User registered successfully", user: { name, email, username, phone, age } });
   }
+
+  const data = []
+  if (pathname === "/login" && method === "POST") {
+    const { username, password, email, phone } = req.body || {};
+    if (!username || !password || !email || !phone) {
+      return res.status(400).json({ message: "Missing fields" });
+    }
+
+    const find = regdata.find(user => user.username === username && user.password === password && user.email === email && user.phone === phone);
+    if (!find) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    data.push({ username, email, phone });
+    console.log(data)
+    return res.status(200).json({ message: "Login successful", user: { username } });
+  }
+
+
 
   // GETCRYPTO
   if (pathname === "/getcrypto" && method === "GET") {
